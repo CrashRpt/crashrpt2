7e9931b7e6f71665f0e61cbb3ea78aa074fefcce
Andreas Schönle <a.schoenle@abberior-instruments.com>
2018-05-09 14:41:16 +0200

    Avoid noexcept() problem for purecall test.
    When trying to throw from the callback (which is a use case to continue
    execution), when inside a noexcept function this fails for
    understandable reasons and triggers another terminate() call. To be able
    to test CR_CPP_PURECALL separately from this, create a test case that
    does not trigger the purecall handler inside a destructor which is by
    default noexcept.

--- reporting/crashrpt/CrashRpt.cpp
+++ reporting/crashrpt/CrashRpt.cpp
@@ -751,130 +751,135 @@ crExceptionFilter(unsigned int code, struct _EXCEPTION_POINTERS* ep)
 // Below crEmulateCrash() related stuff goes
 
 
-class CDerived;
-class CBase
-{
-public:
-    CBase(CDerived *derived): m_pDerived(derived) {};
-    ~CBase();
-    virtual void function(void) = 0;
-
-    CDerived * m_pDerived;
-};
-
-class CDerived : public CBase
-{
-public:
-    CDerived() : CBase(this) {};   // C4355
-    virtual void function(void) {};
-};
-
-CBase::~CBase()
-{
-    m_pDerived -> function();
-}
-
 #include <float.h>
-
 #pragma optimize("g", off)
 
-void sigfpe_test()
+namespace
 {
-    // Code taken from http://www.devx.com/cplus/Article/34993/1954
+	class CBase
+	{
+	public:
+		CBase()
+		{
+			function2();
+		}
+		virtual void function(void) = 0;
+		virtual void function2(void)
+		{
+			function();
+		}
+	};
 
-    //Set the x86 floating-point control word according to what
-    //exceptions you want to trap.
-    _clearfp(); //Always call _clearfp before setting the control
-    //word
-    //Because the second parameter in the following call is 0, it
-    //only returns the floating-point control word
-    unsigned int cw;
+	class Derived : public CBase
+	{
+	public:
+		void function() {};
+	};
+
+	void purecall_test()
+	{
+		Derived d;
+		d.function();
+	}
+
+	void sigfpe_test()
+	{
+		// Code taken from http://www.devx.com/cplus/Article/34993/1954
+
+		//Set the x86 floating-point control word according to what
+		//exceptions you want to trap.
+		_clearfp(); //Always call _clearfp before setting the control
+		//word
+		//Because the second parameter in the following call is 0, it
+		//only returns the floating-point control word
+		unsigned int cw;
 #if _MSC_VER<1400
-    cw = _controlfp(0, 0); //Get the default control
+		cw = _controlfp(0, 0); //Get the default control
 #else
-    _controlfp_s(&cw, 0, 0); //Get the default control
+		_controlfp_s(&cw, 0, 0); //Get the default control
 #endif
-    //word
-    //Set the exception masks off for exceptions that you want to
-    //trap.  When a mask bit is set, the corresponding floating-point
-    //exception is //blocked from being generating.
-    cw &=~(EM_OVERFLOW|EM_UNDERFLOW|EM_ZERODIVIDE|
-        EM_DENORMAL|EM_INVALID);
-    //For any bit in the second parameter (mask) that is 1, the
-    //corresponding bit in the first parameter is used to update
-    //the control word.
-    unsigned int cwOriginal = 0;
+	//word
+	//Set the exception masks off for exceptions that you want to
+	//trap.  When a mask bit is set, the corresponding floating-point
+	//exception is //blocked from being generating.
+		cw &= ~(EM_OVERFLOW | EM_UNDERFLOW | EM_ZERODIVIDE |
+			EM_DENORMAL | EM_INVALID);
+		//For any bit in the second parameter (mask) that is 1, the
+		//corresponding bit in the first parameter is used to update
+		//the control word.
+		unsigned int cwOriginal = 0;
 #if _MSC_VER<1400
-    cwOriginal = _controlfp(cw, MCW_EM); //Set it.
+		cwOriginal = _controlfp(cw, MCW_EM); //Set it.
 #else
-    _controlfp_s(&cwOriginal, cw, MCW_EM); //Set it.
+		_controlfp_s(&cwOriginal, cw, MCW_EM); //Set it.
 #endif
-    //MCW_EM is defined in float.h.
+	//MCW_EM is defined in float.h.
 
-    // Divide by zero
+	// Divide by zero
 
-    float a = 1.0f;
-    float b = 0.0f;
-    float c = a/b;
-    c;
+		float a = 1.0f;
+		float b = 0.0f;
+		float c = a / b;
+		c;
 
-	//Restore the original value when done:
-	_controlfp_s(&cw, cwOriginal, MCW_EM);
-}
+		//Restore the original value when done:
+		_controlfp_s(&cw, cwOriginal, MCW_EM);
+	}
 
-// #pragma optimize("", on)
+	// #pragma optimize("", on)
 
 #define BIG_NUMBER 0x1fffffff
 //#define BIG_NUMBER 0xf
 #pragma warning(disable: 4717) // avoid C4717 warning
 #pragma warning(disable: 4702)
-int RecurseAlloc()
-{
-    int *pi = NULL;
-	for(;;)
-		pi = new int[BIG_NUMBER];
-    return 0;
-}
+	int RecurseAlloc()
+	{
+		int *pi = NULL;
+		for (;;)
+			pi = new int[BIG_NUMBER];
+		return 0;
+	}
 
-// Vulnerable function
+	// Vulnerable function
 #pragma warning(disable : 4996)   // for strcpy use
 #pragma warning(disable : 6255)   // warning C6255: _alloca indicates failure by raising a stack overflow exception. Consider using _malloca instead
 #pragma warning(disable : 6204)   // warning C6204: Possible buffer overrun in call to 'strcpy': use of unchecked parameter 'str'
-void test_buffer_overrun()
-{
-	int a[12];
-	for (int i = 0; i < 16; i++)
+	void test_buffer_overrun()
 	{
-		a[i] = i;
+		int a[12];
+		for (int i = 0; i < 16; i++)
+		{
+			a[i] = i;
+		}
+
+		// No longer crashes in VS2017
+		// char* buffer = (char*)_alloca(10);
+		// strcpy(buffer, str); // overrun buffer !!!
+
+		// use a secure CRT function to help prevent buffer overruns
+		// truncate string to fit a 10 byte buffer
+		// strncpy_s(buffer, _countof(buffer), str, _TRUNCATE);
 	}
-
-    // No longer crashes in VS2017
-    // char* buffer = (char*)_alloca(10);
-	// strcpy(buffer, str); // overrun buffer !!!
-
-    // use a secure CRT function to help prevent buffer overruns
-    // truncate string to fit a 10 byte buffer
-    // strncpy_s(buffer, _countof(buffer), str, _TRUNCATE);
-}
 #pragma warning(default : 4996)
 #pragma warning(default : 6255)
 #pragma warning(default : 6204)
 
-// Stack overflow function
-struct DisableTailOptimization
-{
-       ~DisableTailOptimization() {
-               ++ v;
-       }
-       static int v;
-};
+	// Stack overflow function
+	struct DisableTailOptimization
+	{
+		~DisableTailOptimization() {
+			++v;
+		}
+		static int v;
+	};
 
-int DisableTailOptimization::v = 0;
+	int DisableTailOptimization::v = 0;
 
-static void CauseStackOverflow()
-{
-       DisableTailOptimization v;
-       CauseStackOverflow();
+	static void CauseStackOverflow()
+	{
+		DisableTailOptimization v;
+		CauseStackOverflow();
+	}
 }
 
 CRASHRPTAPI(int)
@@ -908,7 +913,7 @@ crEmulateCrash(unsigned ExceptionType) noexcept(false)
     case CR_CPP_PURE_CALL:
         {
             // pure virtual method call
-            CDerived derived;
+			purecall_test();
         }
         break;
     case CR_CPP_SECURITY_ERROR:
